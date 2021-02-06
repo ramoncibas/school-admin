a2e4f5e699ee2ceb10b77667cb4f87b19e166dc8
Ramon Cibas <ramon.cibas@hotmail.com>
2021-02-05 21:54:00 -0300

    some updates

--- DesignPjEscola/FrmLogin.Designer.cs
+++ DesignPjEscola/FrmLogin.Designer.cs
@@ -34,18 +34,19 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.btnLogin = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(415, 245);
+            this.txtEmail.Location = new System.Drawing.Point(421, 312);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(169, 20);
             this.txtEmail.TabIndex = 0;
             // 
             // txtSenha
             // 
-            this.txtSenha.Location = new System.Drawing.Point(415, 286);
+            this.txtSenha.Location = new System.Drawing.Point(421, 353);
             this.txtSenha.Name = "txtSenha";
             this.txtSenha.Size = new System.Drawing.Size(169, 20);
             this.txtSenha.TabIndex = 1;
@@ -58,7 +59,7 @@
             this.btnCancel.FlatAppearance.BorderSize = 0;
             this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCancel.ForeColor = System.Drawing.Color.White;
-            this.btnCancel.Location = new System.Drawing.Point(509, 333);
+            this.btnCancel.Location = new System.Drawing.Point(515, 400);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 23);
             this.btnCancel.TabIndex = 3;
@@ -71,9 +72,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(9)))), ((int)(((byte)(52)))));
-            this.label1.Location = new System.Drawing.Point(360, 245);
+            this.label1.Location = new System.Drawing.Point(366, 312);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.Size = new System.Drawing.Size(48, 16);
             this.label1.TabIndex = 4;
             this.label1.Text = "Email :";
             // 
@@ -82,9 +83,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(9)))), ((int)(((byte)(52)))));
-            this.label2.Location = new System.Drawing.Point(354, 286);
+            this.label2.Location = new System.Drawing.Point(360, 353);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.Size = new System.Drawing.Size(53, 16);
             this.label2.TabIndex = 5;
             this.label2.Text = "Senha :";
             // 
@@ -95,13 +96,25 @@
             this.btnLogin.FlatAppearance.BorderSize = 0;
             this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(415, 333);
+            this.btnLogin.Location = new System.Drawing.Point(421, 400);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(75, 23);
             this.btnLogin.TabIndex = 6;
             this.btnLogin.Text = "Logar";
             this.btnLogin.UseVisualStyleBackColor = false;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panelLogo.BackColor = System.Drawing.Color.Transparent;
+            this.panelLogo.BackgroundImage = global::DesignPjEscola.Properties.Resources.home_escola;
+            this.panelLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panelLogo.Location = new System.Drawing.Point(381, 77);
+            this.panelLogo.Margin = new System.Windows.Forms.Padding(0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(232, 232);
+            this.panelLogo.TabIndex = 7;
             // 
             // FrmLogin
             // 
@@ -111,6 +124,7 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(211)))), ((int)(((byte)(1)))));
             this.CancelButton = this.btnCancel;
             this.ClientSize = new System.Drawing.Size(950, 600);
+            this.Controls.Add(this.panelLogo);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -137,5 +151,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Panel panelLogo;
     }
 }
